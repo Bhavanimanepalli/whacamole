9e28c744a03fff1830256697c55cf24239751cb6
bhavanimanepalli <bhavanimanepalli6@gmail.com>
2024-05-04 09:41:40 +0530

    whac a mole

---
+++ whacamole.js
@@ -0,0 +1,101 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+let gameStarted = false;
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.id = i.toString();
+    tile.addEventListener("click", selectTile);
+    document.getElementById("board").appendChild(tile);
+  }
+  document.getElementById("start-button").addEventListener("click", startGame);
+  document
+    .getElementById("restart-button")
+    .addEventListener("click", restartGame);
+}
+
+function getRandomTile() {
+  let num = Math.floor(Math.random() * 9);
+  return num.toString();
+}
+
+function startGame() {
+  gameStarted = true;
+  document.getElementById("start-button").style.display = "none";
+  document.getElementById("restart-button").style.display = "block";
+  setMole();
+  setInterval(setMole, 800);
+  setInterval(setPlant, 1000);
+}
+
+function restartGame() {
+  gameStarted = false;
+  document.getElementById("start-button").style.display = "block";
+  document.getElementById("restart-button").style.display = "none";
+  score = 0;
+  document.getElementById("score").innerText = score.toString();
+  currMoleTile = null;
+  currPlantTile = null;
+  gameOver = false;
+}
+
+function setMole() {
+  if (gameOver) {
+    return;
+  }
+
+  if (currMoleTile) {
+    currMoleTile.innerHTML = "";
+  }
+
+  let mole = document.createElement("img");
+  mole.src = "./monty-mole.png";
+
+  let num = getRandomTile();
+  if (currPlantTile && currPlantTile.id == num) {
+    return;
+  }
+
+  currMoleTile = document.getElementById(num);
+  currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+  if (gameOver) {
+    return;
+  }
+  if (currPlantTile) {
+    currPlantTile.innerHTML = "";
+  }
+  let plant = document.createElement("img");
+  plant.src = "./piranha-plant.png";
+
+  let num = getRandomTile();
+  if (currMoleTile && currMoleTile.id == num) {
+    return;
+  }
+
+  currPlantTile = document.getElementById(num);
+  currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+  if (gameOver) {
+    return;
+  }
+  if (this == currMoleTile) {
+    score += 10;
+    document.getElementById("score").innerText = score.toString();
+  } else if (this == currPlantTile) {
+    document.getElementById("score").innerText =
+      "GAME OVER: " + score.toString();
+    gameOver = true;
+  }
+}
